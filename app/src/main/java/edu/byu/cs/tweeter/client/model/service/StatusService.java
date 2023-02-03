2c082127a6f8b33980f7d17cd9c86b24c5ec7dc1
Jake Schilling <jake@schillingcentral.net>
2023-02-02 22:31:08 -0700

    Feed

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -11,9 +11,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.model.backgroundTask.GetFeedTask;
 import edu.byu.cs.tweeter.client.model.backgroundTask.GetStoryTask;
+import edu.byu.cs.tweeter.client.presenter.FeedPresenter;
 import edu.byu.cs.tweeter.client.presenter.StoryPresenter;
-import edu.byu.cs.tweeter.client.view.main.story.StoryFragment;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
@@ -24,8 +25,19 @@ public class StatusService {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(getStoryTask);
     }
+    public void loadMoreItemsFeed(User user, int pageSize, Status lastStatus, FeedPresenter.GetFeedObserver getFeedObserver) {
+        GetStoryTask getStoryTask = new GetStoryTask(Cache.getInstance().getCurrUserAuthToken(),
+                user, pageSize, lastStatus, new GetFeedHandler(getFeedObserver));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getStoryTask);
+    }
 
     public interface GetStoryObserver {
+        void displayError(String message);
+        void displayException(Exception ex);
+        void onSuccess(List<Status> statuses, boolean morePages);
+    }
+    public interface GetFeedObserver {
         void displayError(String message);
         void displayException(Exception ex);
         void onSuccess(List<Status> statuses, boolean morePages);
@@ -52,6 +64,28 @@ public class StatusService {
                 observer.displayError("Failed to get story: " + message);
             } else if (msg.getData().containsKey(GetStoryTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(GetStoryTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
+    public class GetFeedHandler extends Handler {
+        private StatusService.GetFeedObserver observer;
+        public GetFeedHandler(StatusService.GetFeedObserver observe) {
+            super(Looper.getMainLooper());
+            observer = observe;
+        }
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetFeedTask.SUCCESS_KEY);
+            if (success) {
+                List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetFeedTask.STATUSES_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFeedTask.MORE_PAGES_KEY);
+                observer.onSuccess(statuses,hasMorePages);
+            } else if (msg.getData().containsKey(GetFeedTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetFeedTask.MESSAGE_KEY);
+                observer.displayError("Failed to get feed: " + message);
+            } else if (msg.getData().containsKey(GetFeedTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFeedTask.EXCEPTION_KEY);
                 observer.displayException(ex);
             }
         }
--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
@@ -1,4 +1,102 @@
 package edu.byu.cs.tweeter.client.presenter;
 
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.model.service.StatusService;
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.client.view.main.feed.FeedFragment;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.Status;
+import edu.byu.cs.tweeter.model.domain.User;
+
 public class FeedPresenter {
+    private View view;
+    private UserService userService;
+    private StatusService statusService;
+    private Status lastStatus;
+    public FeedPresenter(View view) {
+        this.view = view;
+        userService = new UserService();
+        statusService = new StatusService();
+    }
+    public Status getLastStatus() {
+        return lastStatus;
+    }
+
+    public void setLastStatus(Status lastStatus) {
+        this.lastStatus = lastStatus;
+    }
+
+    public boolean isHasMorePages() {
+        return hasMorePages;
+    }
+
+    public void setHasMorePages(boolean hasMorePages) {
+        this.hasMorePages = hasMorePages;
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+
+    public void setLoading(boolean loading) {
+        isLoading = loading;
+    }
+
+    private boolean hasMorePages;
+    private boolean isLoading = false;
+    private static final int PAGE_SIZE = 10;
+
+
+    public interface View {
+        void displayMessage(String message);
+        void setLoadingFooter(boolean set);
+        void addMoreItems(List<Status> statuses);
+        void userReceived(User user);
+    }
+    public void loadMoreItems(User user) {
+        if (!isLoading) {
+            isLoading = true;
+            view.setLoadingFooter(true);
+            statusService.loadMoreItemsFeed(user,PAGE_SIZE,lastStatus, new FeedPresenter.GetFeedObserver());
+        }
+    }
+
+    public void getUser(AuthToken authToken, String username) {
+        userService.getUser(authToken,username, new FeedPresenter.GetUserObserver());
+    }
+    public class GetUserObserver implements UserService.GetUserObserver {
+
+        @Override
+        public void handleSuccess(User user) {
+            view.userReceived(user);
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            view.displayMessage(message);
+        }
+
+    }
+    public class GetFeedObserver implements StatusService.GetFeedObserver {
+
+        @Override
+        public void displayError(String message) {
+            view.displayMessage(message);
+        }
+
+        @Override
+        public void displayException(Exception ex) {
+            view.displayMessage("Failed to get feed because of exception: " + ex.getMessage());
+        }
+
+        @Override
+        public void onSuccess(List<Status> statuses, boolean morePages) {
+            setLoading(false);
+            view.setLoadingFooter(isLoading);
+            setHasMorePages(morePages);
+            setLastStatus((statuses.size() > 0) ? statuses.get(statuses.size() - 1) : null);
+            view.addMoreItems(statuses);
+        }
+    }
 }
